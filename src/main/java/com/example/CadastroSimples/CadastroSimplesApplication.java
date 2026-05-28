cb12c1396f8c34e4ac7c5faed3fe15b60afab0db
mavini01 <mavinilr@gmail.com>
2026-05-28 17:56:16 -0300

    first commit

---
+++ src/main/java/com/example/CadastroSimples/CadastroSimplesApplication.java
@@ -0,0 +1,13 @@
+package com.example.CadastroSimples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroSimplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroSimplesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CadastroSimples/CadastroSimplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CadastroSimples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroSimplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
